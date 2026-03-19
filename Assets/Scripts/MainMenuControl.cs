d534e5c0d6274955b45a4d68444c88508b071650
NorainAlmajed <202301660@student.polytechnic.bh>
2026-03-19 10:44:00 +0300

    remove line

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -26,7 +26,6 @@ public class MainMenuControl : MonoBehaviour
     void Start()
     {
 
-        PlayerPrefs.DeleteAll();
         StartCoroutine(FadeInTurnOff());
 
         if(hasClicked == true)
